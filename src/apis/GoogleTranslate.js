22b12891e9efe5acdddaefd16ebcdbbb96f3ac22
akshatsinghkaushik <akshatsingh99@gmail.com>
2020-08-13 18:08:40 +1000

    Fix API Key abstraction + Update README.md

--- src/apis/GoogleTranslate.js
+++ src/apis/GoogleTranslate.js
@@ -2,4 +2,7 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL: 'https://translation.googleapis.com/language/translate/v2',
+  params: {
+    key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
+  },
 });
